ea443bf439ab9d341a0f54ed884f8515c8579cab
Jan Scholtyssek <mindleaving@gmail.com>
2020-07-10 23:45:21 +0200

    Added SqlMigrationTools

---
+++ SqlMigrationTools/SqlCredential.cs
@@ -0,0 +1,14 @@
+﻿namespace SqlMigrationTools
+{
+    public class SqlCredential
+    {
+        public SqlCredential(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username { get; }
+        public string Password { get; }
+    }
+}
