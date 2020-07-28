d20b320bb3354c28ca37df9161b7d7893d94fc08
Jan Scholtyssek <mindleaving@gmail.com>
2020-07-28 14:36:22 +0200

    Removed support for collection name from a specific assembly. Use DataApiCollectionAttribute instead.

--- DataAPI.Client/Repositories/IReadonlyObjectDatabase.cs
+++ DataAPI.Client/Repositories/IReadonlyObjectDatabase.cs
@@ -9,7 +9,7 @@ namespace DataAPI.Client.Repositories
     public interface IReadonlyObjectDatabase<T> : IOrderedQueryable<T>
     {
         void DeleteCache();
-        Task<IEnumerable<T>> GetManyAsync(string sqlWhereClause, string orderByClause = null, uint? limit = null);
+        Task<IEnumerable<T>> GetManyAsync(string sqlWhereClause = null, string orderByClause = null, uint? limit = null);
         Task<IEnumerable<T>> GetManyAsync(Expression<Func<T, bool>> filter, string orderByClause = null, uint? limit = null);
         Task<T> GetFromIdAsync(string id);
         Task<bool> ExistsAsync(string id);
