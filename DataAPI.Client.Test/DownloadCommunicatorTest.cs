ebd62598310abb055ab134e7ea9933848288f594
Jan Scholtyssek <mindleaving@gmail.com>
2020-08-27 09:30:31 +0200

    Added Download-method to IDataApiClient

---
+++ DataAPI.Client.Test/DownloadCommunicatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DataAPI.Client.Test
+{
+    [TestFixture]
+    public class DownloadCommunicatorTest
+    {
+        [Test]
+        [Category("IntegrationTest")]
+        public async Task DownloadFileReturnsFilename()
+        {
+            var dataApiClient = new DataApiClient(ApiSetup.ApiConfiguration);
+            dataApiClient.Login();
+
+            var actual = await dataApiClient.Download("DataBlob", "DCF9C0E1FD926E2C6B5E4E9E18001B3F00EEB30B");
+
+            Assert.That(actual.Filename, Is.EqualTo("excelReferenceCache.json"));
+            var data = ToByteArray(actual.Stream);
+            Assert.That(data, Is.Not.Empty);
+        }
+
+        private byte[] ToByteArray(Stream stream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
